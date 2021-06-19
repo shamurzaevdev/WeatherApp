75fcf0ff4a4778804998d2126db690fbc30dab78
Ali <alisham93@bk.ru>
2021-06-20 01:15:26 +0200

    some changes to ViewController

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     
     
     var networkWeatherManager = NetworkWeatherManager()
-    lazy var locationManager : CLLocationManager = {
+    private lazy var locationManager : CLLocationManager = {
         let lm = CLLocationManager()
         
         lm.delegate = self
@@ -22,58 +22,80 @@ class ViewController: UIViewController {
         return lm
     }()
     
-    var cityLabel : UILabel!
-    var temperatureLabel : UILabel!
-    var feelsLikeTemperatureLabel : UILabel!
-    var weatherIconImageView : UIImageView!
+    private let cityLabel : UILabel = {
+        
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "City"
+        label.font = .monospacedSystemFont(ofSize: 20, weight: .light)
+        
+        return label
+    }()
     
-
+    private let temperatureLabel : UILabel = {
+        
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Temperature"
+        label.font = .boldSystemFont(ofSize: 20)
+        
+        return label
+        
+    }()
+    
+    private let feelsLikeTemperatureLabel : UILabel = {
+        
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Feels like"
+        label.font = .italicSystemFont(ofSize: 20)
+        
+        return label
+    }()
+    
+    private let weatherIconImageView : UIImageView = {
+        
+        let imageView = UIImageView(image: UIImage(systemName: "nosign"))
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.tintColor = .systemYellow
+        
+        return imageView
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView(frame: view.bounds)
+            imageView.center = view.center
+            imageView.image = UIImage(named: "IMG_0293.PNG")
+            imageView.contentMode = .scaleToFill
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            return imageView
+        }()
+    
+    private lazy var buttonThird: UIButton = {
+       
+        
+        let button = UIButton(type: UIButton.ButtonType.custom)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "IMG_0307.PNG"), for: .normal)
+        button.addTarget(self, action: #selector(pressBttonUp(sender:)), for: .touchUpInside)
+        
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let background = UIImage(named: "IMG_0293.PNG")
-
-        var imageView : UIImageView!
-        imageView = UIImageView(frame: view.bounds)
-        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
-        imageView.clipsToBounds = true
-        imageView.image = background
-        imageView.center = view.center
         view.addSubview(imageView)
-        
-        weatherIconImageView = UIImageView(image: UIImage(systemName: "nosign"))
-        weatherIconImageView.translatesAutoresizingMaskIntoConstraints = false
-        weatherIconImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-        weatherIconImageView.tintColor = .systemYellow
         view.addSubview(weatherIconImageView)
-        
-        cityLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-        cityLabel.translatesAutoresizingMaskIntoConstraints = false
-        cityLabel.text = "City"
-        cityLabel.font = .monospacedSystemFont(ofSize: 20, weight: .light)
         view.addSubview(cityLabel)
-        
-        cityLabel.topAnchor.constraint(equalTo: imageView.topAnchor, constant: 100).isActive = true
-        cityLabel.centerXAnchor.constraint(equalTo: imageView.centerXAnchor).isActive = true
-        
-        temperatureLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-        temperatureLabel.translatesAutoresizingMaskIntoConstraints = false
-        temperatureLabel.text = "Temperature"
-        temperatureLabel.font = .boldSystemFont(ofSize: 20)
         view.addSubview(temperatureLabel)
-        
-        temperatureLabel.topAnchor.constraint(equalTo: cityLabel.topAnchor, constant: 20).isActive = true
-        temperatureLabel.centerXAnchor.constraint(equalTo: imageView.centerXAnchor).isActive = true
-        
-        feelsLikeTemperatureLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-        feelsLikeTemperatureLabel.translatesAutoresizingMaskIntoConstraints = false
-        feelsLikeTemperatureLabel.text = "Feels Like"
-        feelsLikeTemperatureLabel.font = .italicSystemFont(ofSize: 20)
         view.addSubview(feelsLikeTemperatureLabel)
-        
-        feelsLikeTemperatureLabel.topAnchor.constraint(equalTo: temperatureLabel.topAnchor, constant: 20).isActive = true
-        feelsLikeTemperatureLabel.centerXAnchor.constraint(equalTo: imageView.centerXAnchor).isActive = true
+        view.addSubview(buttonThird)
+        setUpConstraints()
         
         networkWeatherManager.onCompletion = { [weak self] currentWeather in
             guard let self = self else {
@@ -81,27 +103,10 @@ class ViewController: UIViewController {
             }
             self.updateInterfaceWith(weather: currentWeather)}
         
-        let image = UIImage(named: "IMG_0307.PNG")
-        let buttonThird = UIButton(type: UIButton.ButtonType.custom)
-        buttonThird.translatesAutoresizingMaskIntoConstraints = false
-        buttonThird.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
-        buttonThird.setImage(image, for: .normal)
-        buttonThird.addTarget(self, action: #selector(self.pressBttonUp(sender:)), for: .touchUpInside)
+        
 
-        self.view.addSubview(buttonThird)
         
-        buttonThird.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
-        buttonThird.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -50).isActive = true
-        buttonThird.heightAnchor.constraint(equalToConstant: 70).isActive = true
-        buttonThird.widthAnchor.constraint(equalToConstant: 70).isActive = true
         
-        weatherIconImageView.translatesAutoresizingMaskIntoConstraints = false
-        weatherIconImageView.topAnchor.constraint(equalTo: feelsLikeTemperatureLabel.topAnchor, constant: 25).isActive = true
-        weatherIconImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        
-        weatherIconImageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
-        weatherIconImageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
-
         
         
         
@@ -110,18 +115,14 @@ class ViewController: UIViewController {
             locationManager.requestLocation()
         }
         
-        
     }
     
-
     @objc func pressBttonUp(sender: UIButton) {
     
         self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { [unowned self] city in
             self.networkWeatherManager.fetchCurrentWeather(forRequestType: .cityName(city: city))
         }
     }
-    
-    
     
     func updateInterfaceWith(weather: CurrentWeather) {
         
@@ -131,6 +132,36 @@ class ViewController: UIViewController {
             self.feelsLikeTemperatureLabel.text = weather.feelsLikeTemperatureString
             self.weatherIconImageView.image = UIImage(systemName: weather.systemIconNameString)
         }
+    }
+    
+    private func setUpConstraints() {
+        
+        imageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        cityLabel.topAnchor.constraint(equalTo: imageView.topAnchor, constant: 100).isActive = true
+        cityLabel.centerXAnchor.constraint(equalTo: imageView.centerXAnchor).isActive = true
+        
+        temperatureLabel.topAnchor.constraint(equalTo: cityLabel.topAnchor, constant: 20).isActive = true
+        temperatureLabel.centerXAnchor.constraint(equalTo: imageView.centerXAnchor).isActive = true
+        
+        buttonThird.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+        buttonThird.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -50).isActive = true
+        buttonThird.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        buttonThird.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        feelsLikeTemperatureLabel.topAnchor.constraint(equalTo: temperatureLabel.topAnchor, constant: 20).isActive = true
+        feelsLikeTemperatureLabel.centerXAnchor.constraint(equalTo: imageView.centerXAnchor).isActive = true
+        
+        weatherIconImageView.topAnchor.constraint(equalTo: feelsLikeTemperatureLabel.topAnchor, constant: 25).isActive = true
+        weatherIconImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        weatherIconImageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        weatherIconImageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        
     }
 }
 
